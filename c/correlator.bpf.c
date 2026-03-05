fc17c54780b9b2dd03520ba192062795c4346cb9
Monish Kumar <73230556+ItsMonish@users.noreply.github.com>
2026-03-05 19:05:29 +0530

    added tp handlers for connect and open

--- c/correlator.bpf.c
+++ c/correlator.bpf.c
@@ -13,7 +13,7 @@ struct event {
     // Common for everything
     __u8    type;
     pid_t   pid;
-    pid_t   gid;
+    pid_t   tgid;
     __u64   timestamp;
     char    command[32];
 
@@ -37,3 +37,60 @@ struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 2 << 16);       // 2*64 = 128 KB
 } ring_buffer SEC(".maps");
+
+
+SEC("tp/syscalls/sys_enter_openat")
+int record_open(struct trace_event_raw_sys_enter *ctx) {
+    struct event *e = bpf_ringbuf_reserve(&ring_buffer, sizeof(ring_buffer), 0);
+    if (!e) return 0;
+
+    __u64 ids = bpf_get_current_pid_tgid();
+    e->pid = ids >> 32;
+    e->tgid = (__u32)ids;
+    e->type = EVENT_OPEN;
+    e->timestamp = bpf_ktime_get_ns();
+
+    bpf_get_current_comm(&e->command, sizeof(ring_buffer));
+
+    const char *filename = (const char*)&ctx->args[1];
+    bpf_probe_read_user_str(e->fname, sizeof(e->fname), filename);
+
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
+
+
+SEC("tp/syscalls/sys_enter_connect")
+int record_connect(struct trace_event_raw_sys_enter *ctx) {
+    struct event *e = bpf_ringbuf_reserve(&ring_buffer, sizeof(ring_buffer), 0);
+    if (!e) return 0;
+
+    __u64 ids = bpf_get_current_pid_tgid();
+    e->pid = ids >> 32;
+    e->tgid = (__u32)ids;
+    e->type = EVENT_CONNECT;
+    e->timestamp = bpf_ktime_get_ns();
+
+    struct sockaddr *addr = (struct sockaddr*)ctx->args[1];
+    struct sockaddr sa = {};
+    bpf_probe_read_user(&sa, sizeof(sa), addr);
+
+    e->con_family = sa.sa_family;
+
+    if(sa.sa_family == AF_INET) {
+        struct sockaddr_in ipv4 = {};
+        bpf_probe_read_user(&ipv4, sizeof(ipv4), addr);
+
+        e->con_port = ipv4.sin_port;
+        __builtin_memcpy(e->ipv4_addr, &ipv4.sin_addr.s_addr, sizeof(e->ipv4_addr));
+    } else if(sa.sa_family == AF_INET6) {
+        struct sockaddr_in6 ipv6 = {};
+        bpf_probe_read_user(&ipv6, sizeof(ipv6), addr);
+
+        e->con_port = ipv6.sin6_port;
+        __builtin_memcpy(e->ipv6_addr, &ipv6.sin6_addr, sizeof(e->ipv6_addr));
+    }
+
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
